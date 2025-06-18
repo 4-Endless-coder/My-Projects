458daef8861808a3f979855e775b996c955093c5
4-Endless-coder <asheshdash4@gmail.com>
2025-06-18 14:03:16 +0530

    progress bar js fixed

--- Progress Steps/script.js
+++
@@ -1,7 +0,0 @@
-const progress = document.getElementById();
-const prev = document.getElementById();
-const next = document.getElementById();
-const circles = document.querySelectorAll();
-
-let currentActive = 1;
-
