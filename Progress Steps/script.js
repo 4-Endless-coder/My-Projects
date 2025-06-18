0bb3aaa41cd9368090927ef22898527a712db52e
4-Endless-coder <asheshdash4@gmail.com>
2025-06-18 13:41:50 +0530

    progress bar logic added

---
+++ Progress Steps/script.js
@@ -0,0 +1,7 @@
+const progress = document.getElementById();
+const prev = document.getElementById();
+const next = document.getElementById();
+const circles = document.querySelectorAll();
+
+let currentActive = 1;
+
